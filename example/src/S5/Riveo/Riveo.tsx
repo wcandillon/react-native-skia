0b267c122487512d652b9d2b736d570130e59a4b
William Candillon <wcandillon@gmail.com>
2022-09-22 12:48:31 +0200

    :wrench:

--- example/src/S5/Riveo/Riveo.tsx
+++ example/src/S5/Riveo/Riveo.tsx
@@ -11,8 +11,8 @@ import {
   Skia,
   useImage,
   Group,
+  dist,
   Line,
-  rotate,
   useComputedValue,
 } from "@shopify/react-native-skia";
 import React from "react";
@@ -20,50 +20,46 @@ import { Dimensions } from "react-native";
 
 const { width, height } = Dimensions.get("window");
 
+const d = 600;
+const r = 100;
 const source = Skia.RuntimeEffect.Make(`
 uniform shader image;
+uniform vec2 pointer;
+uniform vec2 origin;
 
-half4 main(float2 xy) {   
+float dist(vec2 a, vec2 b) {
+  return sqrt(pow((a.x - b.x), 2) + pow((a.y - b.y), 2));
+}
+
+half4 main(float2 xy) {
+  if (dist(xy, pointer) < 10 || dist(xy, origin) < 10) {
+    return half4(0, 0, 1, 1);
+  }
   return image.eval(xy);
 }`)!;
 
 export const Riveo = () => {
-  const origin = useValue(vec(0, 0));
-  const pointer = useValue(vec(0, 0));
+  const uniforms = useValue({ pointer: vec(0, 0), origin: vec(0, 0) });
   const oslo = useImage(require("../../assets/oslo.jpg"));
   const onTouch = useTouchHandler({
     onStart: ({ y, x }) => {
-      origin.current = vec(x, y);
+      uniforms.current = { ...uniforms.current, origin: vec(x, y) };
     },
     onActive: ({ x, y }) => {
-      pointer.current = vec(x, y);
+      uniforms.current = { ...uniforms.current, pointer: vec(x, y) };
     },
     onEnd: () => {
-      origin.current = vec(0, 0);
-      pointer.current = vec(0, 0);
+      uniforms.current = { pointer: vec(0, 0), origin: vec(0, 0) };
     },
   });
-  const p1 = useComputedValue(() => {
-    const delta = width - origin.current.x;
-    return vec(pointer.current.x + delta, pointer.current.y);
-  }, [pointer]);
-  const p2 = useComputedValue(() => {
-    const delta = width - origin.current.x;
-    return vec(origin.current.x + delta, origin.current.y);
-  }, [origin]);
-  const p3 = useComputedValue(() => {
-    return rotate(vec(p1.current.x, 0), p1.current, 0);
-  }, [p1]);
-  const p4 = useComputedValue(() => {
-    return rotate(vec(p1.current.x, height), p1.current, 0);
-  }, [p1]);
   if (oslo === null) {
     return null;
   }
   return (
     <Canvas style={{ flex: 1 }} onTouch={onTouch}>
+      <Fill color="black" />
       <Fill>
-        <Shader source={source}>
+        <Shader source={source} uniforms={uniforms}>
           <ImageShader
             image={oslo}
             fit="cover"
@@ -71,11 +67,6 @@ export const Riveo = () => {
           />
         </Shader>
       </Fill>
-      <Group color="cyan">
-        <Circle c={p1} r={10} color="red" />
-        <Circle c={p2} r={10} color="green" />
-        <Line p1={p3} p2={p4} style="stroke" strokeWidth={2} />
-      </Group>
     </Canvas>
   );
 };
