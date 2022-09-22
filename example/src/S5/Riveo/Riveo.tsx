e99897161c59f0e6383f1e04808e46467aaadc05
William Candillon <wcandillon@gmail.com>
2022-09-22 21:33:14 +0200

    :wrench:

--- example/src/S5/Riveo/Riveo.tsx
+++ example/src/S5/Riveo/Riveo.tsx
@@ -24,29 +24,48 @@ uniform vec2 origin;
 uniform vec2 resolution;
 
 vec4 point(vec2 v, vec2 xy, vec4 cl) {
-  if (distance(xy, v) < 10) {
+  if (distance(xy, v) < 5) {
     return vec4(0, 0, 1, 1);
   }
   return cl;
 }
 
-vec4 line(vec2 p1, vec2 p2, vec2 xy, vec4 cl) {
-  vec2 p12 = p2 - p1;
-  vec2 p13 = xy - p1;
-  float d = dot(p12, p13) / length(p12); // = length(p13) * cos(angle)
-  vec2 p4 = p1 + normalize(p12) * d;
-  if (length(p4 - xy) < 5) {
-    return vec4(0.0, 1.0, 0.0, 1.0);
+
+// https://www.youtube.com/watch?v=D_Zq6q1gnvw&t=158s
+vec4 line(vec2 a, vec2 b, vec2 p, vec4 cl) {
+  vec2 ba = b - a;
+  vec2 pa = p - a;
+  //float k = clamp(dot(ba, pa) / dot(ba, ba), 0.0, 1.0);
+  float k = dot(ba, pa) / dot(ba, ba);
+  float d = length(pa - ba * k);
+  if (d < 5) {
+    return vec4(0.3, 0.6, 1.0, 1.0);
   }
   return cl;
+}
+
+vec4 perpendicular(vec2 a, vec2 b, vec2 p, vec4 cl) {
+  vec2 ba = b - a;
+  vec2 pa = p - a;
+
+  if (dot(ba, pa) < 0) {
+    return cl;
+  }
+  return vec4(1.0, 0.0, 0.0, 1.0);
 }
 
 half4 main(float2 xy) {
   half4 cl = vec4(0, 0, 0, 1);
   cl = image.eval(xy);
+  cl = line(origin, pointer, xy, cl);
+  cl = perpendicular(pointer, origin, xy, cl);
   cl = point(pointer, xy, cl);
   cl = point(origin, xy, cl);
-  cl = line(origin, pointer, xy, cl);
+
+  //float d = lengthToAxis(pointer, vec2(pointer.y, -pointer.x), xy, cl);
+  // if (d > 50) {
+  //   return vec4(0, 0, 0, 1);
+  // }
   return cl;
 }`)!;
 
