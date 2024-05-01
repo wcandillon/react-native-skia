979c447810f79d1fd52b732a14b2d18a6017e9a3
William Candillon <wcandillon@gmail.com>
2024-05-01 16:08:15 +0200

    :wrench:

--- example/src/Examples/Breathe/Breathe.tsx
+++ example/src/Examples/Breathe/Breathe.tsx
@@ -3,29 +3,10 @@ import {
   gpu,
   SkiaDomView,
 } from "@shopify/react-native-skia";
-import { demo1 } from "./demo1/main";
-
-const triangleVertWGSL = `@vertex
-fn main(
-  @builtin(vertex_index) VertexIndex : u32
-) -> @builtin(position) vec4f {
-  var pos = array<vec2f, 3>(
-    vec2(0.0, 0.5),
-    vec2(-0.5, -0.5),
-    vec2(0.5, -0.5)
-  );
-
-  return vec4f(pos[VertexIndex], 0.0, 1.0);
-}
-`;
-
-const redFragWGSL = `@fragment
-fn main() -> @location(0) vec4f {
-  return vec4(0.3, 0.6, 1.0, 1.0);
-}`;
+import { demo2 } from "./demo2";
 
 const draw = async (ctx: GPUCanvasContext) => {
-  await demo1(ctx);
+  await demo2(ctx);
   ctx.present();
 };
 
---
+++ example/src/Examples/Breathe/demo2.ts
@@ -0,0 +1,77 @@
+import { gpu } from "@shopify/react-native-skia";
+
+const triangleVertWGSL = `@vertex
+fn main(
+  @builtin(vertex_index) VertexIndex : u32
+) -> @builtin(position) vec4f {
+  var pos = array<vec2f, 3>(
+    vec2(0.0, 0.5),
+    vec2(-0.5, -0.5),
+    vec2(0.5, -0.5)
+  );
+
+  return vec4f(pos[VertexIndex], 0.0, 1.0);
+}
+`;
+
+const redFragWGSL = `@fragment
+fn main() -> @location(0) vec4f {
+  return vec4(0.3, 0.6, 1.0, 1.0);
+}`;
+
+
+export const demo2 = async(context: GPUCanvasContext) => {
+  const adapter = await gpu.requestAdapter();
+  const device = await adapter!.requestDevice();
+  const presentationFormat = 'rgba8unorm';
+  context.configure({
+    device,
+    format: presentationFormat,
+    alphaMode: 'premultiplied',
+  });
+  
+  const pipeline = device.createRenderPipeline({
+    layout: 'auto',
+    vertex: {
+      module: device.createShaderModule({
+        code: triangleVertWGSL,
+      }),
+      entryPoint: 'main',
+    },
+    fragment: {
+      module: device.createShaderModule({
+        code: redFragWGSL,
+      }),
+      entryPoint: 'main',
+      targets: [
+        {
+          format: presentationFormat,
+        },
+      ],
+    },
+    primitive: {
+      topology: 'triangle-list',
+    },
+  });
+  
+    const commandEncoder = device.createCommandEncoder();
+    const textureView = context.getCurrentTexture().createView();
+  
+    const renderPassDescriptor: GPURenderPassDescriptor = {
+      colorAttachments: [
+        {
+          view: textureView,
+          clearValue: [1.0, 0.0, 1.0, 1],
+          loadOp: 'clear',
+          storeOp: 'store',
+        },
+      ],
+    };
+  
+    const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+    passEncoder.setPipeline(pipeline);
+    passEncoder.draw(3);
+    passEncoder.end();
+  
+    device.queue.submit([commandEncoder.finish()]);
+  };
