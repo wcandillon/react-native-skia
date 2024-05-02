0c1ebc4584081e35eb34ea2b9610027830975e79
William Candillon <wcandillon@gmail.com>
2024-05-02 15:05:13 +0200

    :wrench:

--- example/src/Examples/Breathe/demo3.tsx
+++ example/src/Examples/Breathe/demo3.tsx
@@ -1,8 +1,4 @@
-import { gpu } from "@shopify/react-native-skia";
-
-export const demo3 = async(context: GPUCanvasContext) => {
-  const adapter = await gpu.requestAdapter();
-  const device = await adapter.requestDevice();
+export const demo3 = async(device: GPUDevice, context: GPUCanvasContext) => {
    // Create an array of data
    const data = new Float32Array([1.0, 2.0, 3.0, 4.0]);
 
@@ -19,10 +15,10 @@ export const demo3 = async(context: GPUCanvasContext) => {
 
   await gpuBuffer.mapAsync(1, 0, data.byteLength); // GPUMapMode.READ
   console.log("mapAsync done");
-   const arrayBuffer = gpuBuffer.getMappedRange(0,  data.byteLength);
+  const arrayBuffer = gpuBuffer.getMappedRange(0,  data.byteLength);
   const readData = new Float32Array(arrayBuffer);
 
   //  // Output the read data
- console.log('Data read from the buffer:', readData);
- gpuBuffer.unmap();
+  console.log('Data read from the buffer:', readData);
+  gpuBuffer.unmap();
   };
