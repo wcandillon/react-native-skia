372d89f8f852289e56b85c8204139edf8d67eeb2
William Candillon <wcandillon@gmail.com>
2024-05-06 09:33:08 +0200

    :wrench:

--- example/src/Examples/Breathe/demo1/demo7.ts
+++ example/src/Examples/Breathe/demo1/demo7.ts
@@ -3,110 +3,160 @@ import { Dimensions } from "react-native";
 const { width, height } = Dimensions.get("window");
 
 export const demo7 = async (device: GPUDevice, context: GPUCanvasContext, textureData: Uint8Array) => {
- 
-    const swapChainFormat = 'bgra8unorm';
 
-    const vertexShaderModule = device.createShaderModule({
-      code: `
-      @vertex
-        fn main( @location(0) position: vec2f) -> @builtin(position) vec4f {
-          return vec4f(position, 0.0, 1.0);
-        }
-      `,
-    });
-  
-    const fragmentShaderModule = device.createShaderModule({
-      code: `
-@group(0) @binding(0) var mySampler: sampler;
-@group(0) @binding(1) var myTexture: texture_2d<f32>;
+  const presentationFormat = 'bgra8unorm';
 
-      @fragment
-      fn main(@location(0) fragUV: vec2f) -> @location(0) vec4f {
-        return textureSample(myTexture, mySampler, fragUV);
-      }
-      `,
-    });
-  
-    const pipeline = device.createRenderPipeline({
-      layout: "auto",
-      vertex: {
-        module: vertexShaderModule,
-        entryPoint: 'main',
-      },
-      fragment: {
-        module: fragmentShaderModule,
-        entryPoint: 'main',
-        targets: [
-          {
-            format: swapChainFormat,
-          },
-        ],
-      },
-      primitive: {
-        topology: 'triangle-list',
-      },
-    });
-  
-    const texture = device.createTexture({
-      size: {
-        width: width,
-        height: height,
-        depthOrArrayLayers: 1,
-      },
-      format: 'rgba8unorm',
-      usage: GPUTextureUsage.COPY_DST | GPUTextureUsage.RENDER_ATTACHMENT,
-    });
-  
-    const textureView = texture.createView();
-  
-  
-    device.queue.writeTexture(
-      { texture: texture, mipLevel: 0, origin: { x: 0, y: 0, z: 0 } },
-      textureData.buffer,
-      {
-        offset: 0,
-        bytesPerRow: 4 * width,
-        rowsPerImage: height,
-      },
-      { width, height }
-    );
-  
-    const renderPassDescriptor: GPURenderPassDescriptor = {
-      colorAttachments: [
-        {
-          view: context.getCurrentTexture().createView(),
-          clearValue: [0.5, 0.5, 0.5, 1.0],
-          loadOp: 'clear',
-          storeOp: 'store',
-        },
-      ],
+  const pixels = new Uint8Array(256 * 256 * 4);
+  pixels.fill(255);
+  let i = 0;
+  for (let x = 0; x < 256 * 4; x++) {
+    for (let y = 0; y < 256 * 4; y++) {
+      pixels[i++] = (x * y) % 255;
+    }
+  }
+
+  context.configure({
+    device,
+    format: presentationFormat,
+  });
+
+  const module = device.createShaderModule({
+    label: 'our hardcoded textured quad shaders',
+    code: `
+    struct OurVertexShaderOutput {
+      @builtin(position) position: vec4f,
+      @location(0) texcoord: vec2f,
     };
+
+    @vertex fn vs(
+      @builtin(vertex_index) vertexIndex : u32
+    ) -> OurVertexShaderOutput {
+      let pos = array(
+        // 1st triangle
+        vec2f( 0.0,  0.0),  // center
+        vec2f( 1.0,  0.0),  // right, center
+        vec2f( 0.0,  1.0),  // center, top
+
+        // 2st triangle
+        vec2f( 0.0,  1.0),  // center, top
+        vec2f( 1.0,  0.0),  // right, center
+        vec2f( 1.0,  1.0),  // right, top
+      );
+
+      var vsOutput: OurVertexShaderOutput;
+      let xy = pos[vertexIndex];
+      vsOutput.position = vec4f(xy, 0.0, 1.0);
+      vsOutput.texcoord = xy;
+      return vsOutput;
+    }
+
+    @group(0) @binding(0) var ourSampler: sampler;
+    @group(0) @binding(1) var ourTexture: texture_2d<f32>;
+
+    @fragment fn fs(fsInput: OurVertexShaderOutput) -> @location(0) vec4f {
+      return textureSample(ourTexture, ourSampler, fsInput.texcoord);
+    }
+  `,
+  });
+
+  const pipeline = device.createRenderPipeline({
+    label: 'hardcoded textured quad pipeline',
+    layout: 'auto',
+    vertex: {
+      module,
+      entryPoint: "vs"
+    },
+    fragment: {
+      module,
+      targets: [{ format: presentationFormat }],
+      entryPoint: "fs"
+    },
+    primitive: {
+      topology: "triangle-list",
+    },
+  });
+
+  const url =
+    'https://webgpufundamentals.org/webgpu/resources/images/f-texture.png';
+  const texture = device.createTexture({
+    label: url,
+    format: 'rgba8unorm',
+    size: { width: 256, height: 256 },
+    usage:
+      GPUTextureUsage.TEXTURE_BINDING |
+      GPUTextureUsage.COPY_DST |
+      GPUTextureUsage.RENDER_ATTACHMENT,
+  });
+  device.queue.writeTexture(
+    { texture: texture, mipLevel: 0, origin: { x: 0, y: 0, z: 0 } },
+    pixels.buffer,
+    {
+      offset: 0,
+      bytesPerRow: 4 * 256,
+      rowsPerImage: 256,
+    },
+    { width: 256, height: 256 }
+  );
+
+  const bindGroups: GPUBindGroup[] = [];
+  for (let i = 0; i < 8; ++i) {
     const sampler = device.createSampler({
-      addressModeU: "repeat",
-      addressModeV: "repeat",
-      magFilter: "linear",
-      minFilter: "linear",
-      mipmapFilter: "linear",
+      addressModeU: i & 1 ? 'repeat' : 'clamp-to-edge',
+      addressModeV: i & 2 ? 'repeat' : 'clamp-to-edge',
+      magFilter: i & 4 ? 'linear' : 'nearest',
     });
-    const commandEncoder = device.createCommandEncoder();
-    const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
-    passEncoder.setPipeline(pipeline);
-    passEncoder.setBindGroup(0, device.createBindGroup({
+
+    const bindGroup = device.createBindGroup({
       layout: pipeline.getBindGroupLayout(0),
       entries: [
-        {
-          binding: 0,
-          resource: textureView,
-        },
-        {
-          binding: 1,
-          resource: sampler,
-        },
+        { binding: 0, resource: sampler },
+        { binding: 1, resource: texture.createView() },
       ],
-    }));
-    passEncoder.draw(3, 1, 0, 0);
-    passEncoder.end();
-  
-    device.queue.submit([commandEncoder.finish()]);
-    context.present();
+    });
+    bindGroups.push(bindGroup);
+  }
+
+  const renderPassDescriptor: GPURenderPassDescriptor = {
+    label: 'our basic canvas renderPass',
+    colorAttachments: [
+      {
+        // view: <- to be filled out when we render
+        clearValue: [0.3, 0.3, 0.3, 1],
+        loadOp: 'clear',
+        storeOp: 'store',
+      },
+    ],
+  };
+
+  const settings = {
+    addressModeU: 'repeat',
+    addressModeV: 'repeat',
+    magFilter: 'linear',
+  };
+
+  function render() {
+    const ndx =
+      (settings.addressModeU === 'repeat' ? 1 : 0) +
+      (settings.addressModeV === 'repeat' ? 2 : 0) +
+      (settings.magFilter === 'linear' ? 4 : 0);
+    const bindGroup = bindGroups[ndx];
+
+    // Get the current texture from the canvas context and
+    // set it as the texture to render to.
+    renderPassDescriptor.colorAttachments[0].view = context
+      .getCurrentTexture()
+      .createView();
+
+    const encoder = device.createCommandEncoder();
+    const pass = encoder.beginRenderPass(renderPassDescriptor);
+    pass.setPipeline(pipeline);
+    pass.setBindGroup(0, bindGroup);
+    pass.draw(6); // call our vertex shader 6 times
+    pass.end();
+
+    const commandBuffer = encoder.finish();
+    device.queue.submit([commandBuffer]);
+  }
+
+  render();
 };
