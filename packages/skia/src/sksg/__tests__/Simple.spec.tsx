374eab30e274c9f202b27bf7d2182cb304d8271b
William Candillon <wcandillon@gmail.com>
2024-12-01 21:46:41 +0100

    :wrench:

--- packages/skia/src/sksg/__tests__/Simple.spec.tsx
+++ packages/skia/src/sksg/__tests__/Simple.spec.tsx
@@ -2,15 +2,20 @@ import React from "react";
 
 import { importSkia } from "../../renderer/__tests__/setup";
 import { SkiaRoot } from "../Reconciler";
+import { checkImage } from "../../__tests__/setup";
 
 describe("Simple", () => {
   it("should have a simple render", () => {
     const { Skia } = importSkia();
     const root = new SkiaRoot();
-    root.render(<skCircle r={10} />);
+    root.render(<skCircle r={128} />);
     const surface = Skia.Surface.Make(768, 768)!;
     expect(surface).toBeDefined();
     const canvas = surface.getCanvas();
     root.draw({ Skia, canvas });
+    surface.flush();
+    const image = surface.makeImageSnapshot();
+    expect(image).toBeDefined();
+    checkImage(image, "snapshots/sksg/simple.png");
   });
 });
