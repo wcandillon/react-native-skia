8819bba2907a319c45011118aad756b8c5ea6026
William Candillon <wcandillon@gmail.com>
2024-12-10 12:04:30 +0100

    :wrench:

--- packages/skia/src/sksg/DrawingContext.ts
+++ packages/skia/src/sksg/DrawingContext.ts
@@ -11,6 +11,44 @@ import {
   type SkPaint,
 } from "../skia/types";
 
+import { isDeclarationNode, type Node } from "./Node";
+
+interface ContextProcessingResult {
+  shouldRestoreMatrix: boolean;
+  shouldRestorePaint: boolean;
+}
+
+export const preProcessContext = (
+  ctx: DrawingContext,
+  props: PaintProps & TransformProps,
+  children: Node<unknown>[]
+) => {
+  "worklet";
+  const shouldRestoreMatrix = ctx.processMatrix(props);
+  ctx.declCtx.save();
+  children.forEach((child) => {
+    if (isDeclarationNode(child)) {
+      child.declare(ctx.declCtx);
+    }
+  });
+  ctx.declCtx.restore();
+  const shouldRestorePaint = ctx.processPaint(props);
+  return { shouldRestoreMatrix, shouldRestorePaint };
+};
+
+export const postProcessContext = (
+  ctx: DrawingContext,
+  { shouldRestoreMatrix, shouldRestorePaint }: ContextProcessingResult
+) => {
+  "worklet";
+  if (shouldRestoreMatrix) {
+    ctx.canvas.restore();
+  }
+  if (shouldRestorePaint) {
+    ctx.restore();
+  }
+};
+
 export class DrawingContext {
   private paints: SkPaint[];
   public declCtx: DeclarationContext;
--- packages/skia/src/sksg/DrawingNodes.ts
+++ packages/skia/src/sksg/DrawingNodes.ts
@@ -2,9 +2,34 @@ import { processCircle } from "../dom/nodes";
 import type { CircleProps, DrawingNodeProps, GroupProps } from "../dom/types";
 import type { Vector } from "../skia/types";
 
-import type { DrawingContext } from "./DrawingContext";
-import { isDeclarationNode, isDrawingNode, NodeType } from "./Node";
+import {
+  postProcessContext,
+  preProcessContext,
+  type DrawingContext,
+} from "./DrawingContext";
+import { isDrawingNode, NodeType } from "./Node";
 import type { DrawingNode, Node } from "./Node";
+
+export class GroupNode implements DrawingNode<GroupProps> {
+  type = NodeType.Drawing as const;
+  constructor(private props: GroupProps) {}
+
+  children: Node<unknown>[] = [];
+
+  draw(ctx: DrawingContext) {
+    const result = preProcessContext(ctx, this.props, this.children);
+    this.children.forEach((child) => {
+      if (isDrawingNode(child)) {
+        child.draw(ctx);
+      }
+    });
+    postProcessContext(ctx, result);
+  }
+
+  clone() {
+    return new GroupNode(this.props);
+  }
+}
 
 export class CircleNode implements DrawingNode<CircleProps> {
   type = NodeType.Drawing as const;
@@ -18,20 +43,9 @@ export class CircleNode implements DrawingNode<CircleProps> {
 
   draw(ctx: DrawingContext) {
     const { canvas } = ctx;
-    // TODO: refactor to use group mixin
-    const shouldRestoreMatrix = ctx.processMatrix(this.props);
-    const shouldRestorePaint = ctx.processPaint(this.props);
+    const result = preProcessContext(ctx, this.props, this.children);
     canvas.drawCircle(this.c.x, this.c.y, this.props.r, ctx.paint);
-    if (shouldRestoreMatrix) {
-      canvas.restore();
-    }
-    if (shouldRestorePaint) {
-      ctx.restore();
-    }
-  }
-
-  protected deriveProps() {
-    return processCircle(this.props).c;
+    postProcessContext(ctx, result);
   }
 
   clone() {
@@ -51,49 +65,8 @@ export class FillNode implements DrawingNode<DrawingNodeProps> {
 
   draw(ctx: DrawingContext) {
     const { canvas } = ctx;
-    const shouldRestoreMatrix = ctx.processMatrix(this.props);
-    const shouldRestorePaint = ctx.processPaint(this.props);
+    const result = preProcessContext(ctx, this.props, this.children);
     canvas.drawPaint(ctx.paint);
-    if (shouldRestoreMatrix) {
-      canvas.restore();
-    }
-    if (shouldRestorePaint) {
-      ctx.restore();
-    }
-  }
-}
-
-export class GroupNode implements DrawingNode<GroupProps> {
-  type = NodeType.Drawing as const;
-  constructor(private props: GroupProps) {}
-
-  children: Node<unknown>[] = [];
-
-  draw(ctx: DrawingContext) {
-    const { canvas } = ctx;
-    const shouldRestoreMatrix = ctx.processMatrix(this.props);
-    ctx.declCtx.save();
-    this.children.forEach((child) => {
-      if (isDeclarationNode(child)) {
-        child.declare(ctx.declCtx);
-      }
-    });
-    ctx.declCtx.restore();
-    const shouldRestorePaint = ctx.processPaint(this.props);
-    this.children.forEach((child) => {
-      if (isDrawingNode(child)) {
-        child.draw(ctx);
-      }
-    });
-    if (shouldRestoreMatrix) {
-      canvas.restore();
-    }
-    if (shouldRestorePaint) {
-      ctx.restore();
-    }
-  }
-
-  clone() {
-    return new GroupNode(this.props);
+    postProcessContext(ctx, result);
   }
 }
