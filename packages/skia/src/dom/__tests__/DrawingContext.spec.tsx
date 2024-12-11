6c41b3fd29823b77f020e6ffe6e2821a60cd4e0e
William Candillon <wcandillon@gmail.com>
2024-12-11 17:32:00 +0100

    :wrench:

--- packages/skia/src/dom/__tests__/DrawingContext.spec.tsx
+++ packages/skia/src/dom/__tests__/DrawingContext.spec.tsx
@@ -31,69 +31,51 @@ describe("DrawingContext", () => {
     expect(ctx.paint).toBe(rootPaint);
   });
 
-  // it("should only create a context if necessary", () => {
-  //   const { canvas } = setupSkia(width, height);
-  //   const { Skia } = importSkia();
-  //   const Sk = getSkDOM();
-  //   const group = Sk.Group();
-  //   group.addChild(Sk.Group({ color: "red" }));
-  //   const ctx = new JsiDrawingContext(Skia, canvas);
-  //   const rootPaint = ctx.paint;
-  //   expect(rootPaint).toBeDefined();
+  it("should only create a context if necessary", () => {
+    const { canvas } = setupSkia(width, height);
+    const { Skia } = importSkia();
+    const Sk = getSkDOM();
+    const group = Sk.Group();
+    group.addChild(Sk.Group({ color: "red" }));
+    const ctx = new JsiDrawingContext(Skia, canvas);
+    const rootPaint = ctx.paint;
+    expect(rootPaint).toBeDefined();
 
-  //   let shouldRestore = ctx.saveAndConcat(group);
-  //   expect(shouldRestore).toBe(false);
-  //   expect(ctx.paint).toBe(rootPaint);
-  //   expect(ctx.paint.getColor()).toEqual(Skia.Color("black"));
+    const { shouldRestorePaint } = preProcessContext(
+      ctx,
+      group.getProps(),
+      group.children()
+    );
+    expect(shouldRestorePaint).toBe(false);
+    expect(ctx.paint).toBe(rootPaint);
+    expect(ctx.paint.getColor()).toEqual(Skia.Color("black"));
 
-  //   shouldRestore = ctx.saveAndConcat(group.children()[0] as Node<PaintProps>);
-  //   expect(shouldRestore).toBe(true);
-  //   expect(ctx.paint).not.toBe(rootPaint);
-  //   expect(ctx.paint.getColor()).toEqual(Skia.Color("red"));
-  // });
+    const r = preProcessContext(
+      ctx,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      group.children()[0].getProps() as any,
+      group.children()[0].children()
+    );
+    expect(r.shouldRestorePaint).toBe(true);
+    expect(ctx.paint).not.toBe(rootPaint);
+    expect(ctx.paint.getColor()).toEqual(Skia.Color("red"));
+  });
 
-  // it("should only visit declarations if needed", () => {
-  //   const { canvas } = setupSkia(width, height);
-  //   const { Skia } = importSkia();
-  //   const Sk = getSkDOM();
-  //   const group = Sk.Group({ color: "red" });
-  //   const ctx = new JsiDrawingContext(Skia, canvas);
-  //   const rootPaint = ctx.paint;
-  //   expect(rootPaint).toBeDefined();
+  it("should only visit declarations if needed", () => {
+    const { canvas } = setupSkia(width, height);
+    const { Skia } = importSkia();
+    const Sk = getSkDOM();
+    const group = Sk.Group({ color: "red" });
+    const ctx = new JsiDrawingContext(Skia, canvas);
+    const rootPaint = ctx.paint;
+    expect(rootPaint).toBeDefined();
 
-  //   ctx.saveAndConcat(group);
-  //   const cachedPaint = ctx.paint;
-  //   ctx.restore();
+    preProcessContext(ctx, group.getProps(), group.children());
+    const cachedPaint = ctx.paint;
+    ctx.restore();
 
-  //   ctx.saveAndConcat(group);
-  //   expect(ctx.paint).not.toBe(cachedPaint);
-  //   ctx.restore();
-  // });
-
-  // it("should keep a paint stable if the parent doesn't change", () => {
-  //   const { canvas } = setupSkia(width, height);
-  //   const { Skia } = importSkia();
-  //   const Sk = getSkDOM();
-  //   const root = Sk.Group({});
-  //   const child = Sk.Group({ color: "red" });
-  //   root.addChild(child);
-
-  //   const ctx = new JsiDrawingContext(Skia, canvas);
-  //   const rootPaint = ctx.paint;
-  //   expect(rootPaint).toBeDefined();
-
-  //   ctx.saveAndConcat(root);
-
-  //   ctx.saveAndConcat(child);
-  //   const p1 = ctx.paint;
-  //   ctx.restore();
-
-  //   ctx.saveAndConcat(child);
-  //   const p2 = ctx.paint;
-  //   ctx.restore();
-
-  //   ctx.restore();
-
-  //   expect(p1).not.toBe(p2);
-  // });
+    preProcessContext(ctx, group.getProps(), group.children());
+    expect(ctx.paint).not.toBe(cachedPaint);
+    ctx.restore();
+  });
 });
